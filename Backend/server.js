eccc874a7516a9c5fdc914a6b219f026cecfd0c1
OsamaQureshi147 <osamahsan@gmail.com>
2021-04-28 15:28:11 +0500

    Routes defined

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db')
 
 
+//Connect Database
+connectDB();
 
-app.get('/', (req, res) => res.send('API Running...'))
+app.get('/', (req, res) => res.send('API Running...'));
 const PORT = process.env.PORT || 5000;
+
+//Define ROutes
+app.use('/api/users', require('./Routes/api/users'));
+app.use('/api/auth', require('./Routes/api/auth'));
+app.use('/api/profile', require('./Routes/api/profile'));
+app.use('/api/post', require('./Routes/api/post'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
